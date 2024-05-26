dcda751030172447b0165bbb4f476a9f7c02ec9c
Jose Luis Monteagudo <jl_monteagudo@yahoo.es>
2024-05-26 12:37:07 +0200

    feat: allow filter tickers

--- src/app/pages/tickers/tickers.component.ts
+++ src/app/pages/tickers/tickers.component.ts
@@ -1,13 +1,13 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TickerService } from '../../shared/services/ticker.service';
 import { StrategyService } from '../../shared/services/strategy.service';
-
 @Component({
   selector: 'app-tickers',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './tickers.component.html',
   styleUrl: './tickers.component.scss',
 })
@@ -18,4 +18,20 @@ export class TickersComponent {
   candleInterval = this.#strategyService.candleInterval;
 
   tickers = this.#tickerService.tickers;
+
+  searchTerm = signal('');
+  filterGroup = signal('USDT');
+
+  filteredTickers = computed(() => {
+    const searchTerm = this.searchTerm().toUpperCase();
+    let tickers = [...(this.tickers() || [])];
+
+    if (this.filterGroup() === 'USDT')
+      tickers = tickers.filter((ticker) => ticker.symbol.endsWith('/USDT'));
+
+    if (this.searchTerm().length)
+      tickers = tickers.filter((ticker) => ticker.symbol.includes(searchTerm));
+
+    return tickers;
+  });
 }
